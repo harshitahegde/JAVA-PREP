c0a9b90c049189e69f2bf35bc1036fd90048ec9d
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 17:02:20 +0530

    Pyramid pattern program

--- Module1_JavaBasics/PyramidPattern.java
+++ Module1_JavaBasics/PyramidPattern.java
@@ -1,5 +1,21 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class PyramidPattern {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of rows and coloumns");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int s=i;s<n;s++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=i*2-1;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
     
 }
