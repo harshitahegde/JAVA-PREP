70c6e83a6dc3e989fce39078ab21a44564c20568
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 19:15:59 +0530

    Added polymorphism example with Shape, Circle, and Rectangle

--- Module3_Oops/OverloadingVsOverriding.java
+++ Module3_Oops/OverloadingVsOverriding.java
@@ -1,5 +1,41 @@
 package Module3_Oops;
+class Shape {
+    void area() {
+        System.out.println("Area:");
+    }
+}
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    void area() {
+        System.out.println("Area of Circle: " + (3.14 * radius * radius));
+    }
+}
+class Rectangle extends Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    void area() {
+        System.out.println("Area of Rectangle: " + (length * width));
+    }
+}
 
 public class OverloadingVsOverriding {
-    
+    public static void main(String[] args) {
+        Shape s1 = new Circle(6);    
+        Shape s2 = new Rectangle(6, 6); 
+        s1.area();  
+        s2.area(); 
+    }
 }
+
