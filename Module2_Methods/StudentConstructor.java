14451594dc7eec82ca4a9dd62df74d0d0b16a664
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 18:55:25 +0530

    Added StudentConstructor demo with default and parameterized constructors

--- Module2_Methods/StudentConstructor.java
+++ Module2_Methods/StudentConstructor.java
@@ -1,5 +1,27 @@
 package Module2_Methods;
 
 public class StudentConstructor {
+    int n;
+
+    public StudentConstructor() {
+         n=9;
+    }
+    StudentConstructor(int n){
+        this.n=n;
+
+    }
+    int display(){
+     return n;
+    }
+    public static void main(String[] args) {
+      StudentConstructor o1=new StudentConstructor(8);
+      System.out.println("enter student number "+ o1.display());
+       StudentConstructor o2=new StudentConstructor();
+       System.out.println("enter student number "+ o2.display());
+
+    
+
+        
+    }
     
 }
