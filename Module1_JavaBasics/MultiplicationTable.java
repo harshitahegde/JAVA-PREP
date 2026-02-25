69bcbc13fb48d053c15215406c06bf442298d338
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 07:59:26 +0530

    Module 1: Multiplication table using for loop

--- Module1_JavaBasics/MultiplicationTable.java
+++ Module1_JavaBasics/MultiplicationTable.java
@@ -1,5 +1,17 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class MultiplicationTable {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int answer=0;
+        int a =sc.nextInt();
+        for(int i=1;i<=10;i++){
+            answer=a*i;
+            System.out.println(a +"*"+i +"is"+ answer);
+            
+        }
+    }
     
 }
