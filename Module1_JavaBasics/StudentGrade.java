bc7b3b31e064c360b665d30e15cd8a2cba555af4
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 17:19:14 +0530

    Pyramid pattern program

--- Module1_JavaBasics/StudentGrade.java
+++ Module1_JavaBasics/StudentGrade.java
@@ -1,5 +1,37 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class StudentGrade {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the student marks");
+        int M=sc.nextInt();
+        switch(M/10){
+            case 10:
+                case 9:{
+                   System.out.println("A");
+                   break;
+                
+        }
+        case 8:{
+            System.out.println("B");
+            break;
+        }
+        case 7:{
+            System.out.println("C");
+            break;
+        }
+        case 6:{
+            System.out.println("D");
+            break;
+        }
+        default:{
+            System.out.println("F");
+            break;
+        }
+        }
+
+   
+    }
     
 }
