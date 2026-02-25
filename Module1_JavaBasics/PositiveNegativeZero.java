94e0afc2396da476ee03845dc155701cf3a3ef23
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 07:53:06 +0530

    Module 1: Positive, Negative or Zero program

--- Module1_JavaBasics/PositiveNegativeZero.java
+++ Module1_JavaBasics/PositiveNegativeZero.java
@@ -1,5 +1,23 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class PositiveNegativeZero {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a =sc.nextInt();
+        if(a==0){
+            System.out.println(a +"is zero");
+        }
+        else{
+            if(a>0){
+                System.out.println(a +"is positive");
+            }else{
+                System.out.println(a +"is negative");
+            }
+        }
+        sc.close();
+
+    }
     
 }
