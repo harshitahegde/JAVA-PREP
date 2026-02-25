2f04b753432bc48421fface4ef5cd9479c46c579
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 08:12:17 +0530

    Module 1: Prime number program

--- Module1_JavaBasics/PrimeNumber.java
+++ Module1_JavaBasics/PrimeNumber.java
@@ -1,5 +1,27 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=sc.nextInt();
+        int count=0;
+        if(a<=1){
+            System.out.println(a+ "is not a prime");
+        }
+        for(int i=2;i<a;i++){
+            if((a%i)==0){
+                System.out.println(a + " is not prime number");
+                count++;  
+                break;
+            }
+
+        }
+        if(count==0){
+        System.out.println(a + "is a prime number");}
+        
+
+    }
     
 }
