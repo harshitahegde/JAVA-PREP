624b1a3b44a4e2ce11651707483c51429a3a8089
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 07:48:39 +0530

    Module 1: Largest of three numbers using if-else

--- Module1_JavaBasics/LargestOfThree.java
+++ Module1_JavaBasics/LargestOfThree.java
@@ -1,5 +1,31 @@
 package Module1_JavaBasics;
+import java.util.*;
 
 public class LargestOfThree {
-    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first digit");
+        int a=sc.nextInt();
+        System.out.println("enter second digit");
+        int b=sc.nextInt();
+        System.out.println("enter third digit");
+        int c=sc.nextInt();
+        if(a>b){
+            if(a>c){
+                System.out.println("a is largest");
+            }
+            else{
+                System.out.println("c is largest");
+            }
+        }
+        else{
+            if(b>c){
+                System.out.println("b is largest");
+            }
+            else{
+                System.out.println("c is largest");
+            }
+        }
+
+    }
 }
