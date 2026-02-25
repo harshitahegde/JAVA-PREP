be8f595d39ef3fef9d287436464d5e6b192a7a93
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 17:26:04 +0530

    Added sum of even numbers using while loop

--- Module1_JavaBasics/SumEvenNumbers.java
+++ Module1_JavaBasics/SumEvenNumbers.java
@@ -1,5 +1,17 @@
 package Module1_JavaBasics;
 
 public class SumEvenNumbers {
+    public static void main(String[] args) {
+        int sum=0;
+        int i=1;
+        while( i<101 && i>0){
+            if(i%2==0){
+                sum+=i;
+            }
+            i++;
+        }
+        System.out.println(""+sum);
+        
+    }
     
 }
