1f30b15c72af76cb600d373d1535a97552c8d70c
Harshita Hegde <hashitavhegde1234@gmail.com>
2026-02-25 18:07:53 +0530

    Added factorial using recursion

--- Module2_Methods/FactorialRecursion.java
+++ Module2_Methods/FactorialRecursion.java
@@ -1,5 +1,29 @@
 package Module2_Methods;
+import java.util.*;
 
 public class FactorialRecursion {
+    static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        if(n<0){
+        System.out.println("invalid number");
+        }
+        else{
+            System.out.println("Factorial of a number is"+fact(n) );
+        }
+      
+        }
+    }
+
     
-}
+
